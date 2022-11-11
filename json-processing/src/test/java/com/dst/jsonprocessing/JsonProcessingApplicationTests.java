f51770de1736c008be8440f96cc33b96b9d478cd
Dimitar Todorov <d.todorov.vn@gmail.com>
2022-11-11 15:44:23 +0200

    Add initial folder structure for JSON processing - Products Shop exercise

---
+++ json-processing/src/test/java/com/dst/jsonprocessing/JsonProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dst.jsonprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonProcessingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
